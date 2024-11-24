6050e0bffb647b81a5c8728b4e65e228440a44b5
Mátyás Paluska <paluskamatyas@gmail.com>
2024-11-24 01:55:25 +0100

    new textures slower gameplay

--- src/main/java/org/jsnake/Board.java
+++ src/main/java/org/jsnake/Board.java
@@ -19,6 +19,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.image.*;
+import java.awt.Graphics2D;
 
 public class Board extends JPanel implements ActionListener {
     
@@ -26,12 +27,12 @@ public class Board extends JPanel implements ActionListener {
         UP, DOWN, LEFT, RIGHT
     }
     
-    private static final int B_WIDTH = 800;
-    private static final int B_HEIGHT = 800;
-    private static final int SQUARE_SIZE = 20;
+    private static final int SQUARE_SIZE = 16;
+    private static final int B_WIDTH = SQUARE_SIZE * 40;
+    private static final int B_HEIGHT = SQUARE_SIZE * 40;
     private static final int MAXLEN = 900;
     private static final int RAND_POS = 29;
-    private static final int DELAY = 60;
+    private static final int DELAY = 100;
 
     private final int x[] = new int[MAXLEN];
     private final int y[] = new int[MAXLEN];
@@ -46,6 +47,9 @@ public class Board extends JPanel implements ActionListener {
     private Image ball;
     private Image apple;
     private Image head;
+    private Image tail;
+    private Image bodyStraight;
+    private Image bodyBent;
 
     public Board() {
         
@@ -64,30 +68,16 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void loadImages() {
-
-        BufferedImage iid = null;
-        try{
-            iid = ImageIO.read(new File("src/main/resources/dot.png"));
+        try {
+            ball = ImageIO.read(new File("src/main/resources/dot.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
+            apple = ImageIO.read(new File("src/main/resources/apple.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
+            head = ImageIO.read(new File("src/main/resources/snake_head.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
+            tail = ImageIO.read(new File("src/main/resources/snake_tail.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
+            bodyStraight = ImageIO.read(new File("src/main/resources/snake_body_straight.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
+            bodyBent = ImageIO.read(new File("src/main/resources/snake_body_bent.png")).getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ball = iid.getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
-
-        BufferedImage iia = null;
-        try{
-            iia = ImageIO.read(new File("src/main/resources/apple.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        apple = iia.getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
-
-        BufferedImage iih = null;
-        try{
-            iih = ImageIO.read(new File("src/main/resources/head.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        head = iih.getScaledInstance(SQUARE_SIZE, SQUARE_SIZE, Image.SCALE_SMOOTH);
     }
 
     private void initGame() {
@@ -95,8 +85,8 @@ public class Board extends JPanel implements ActionListener {
         length = 3;
 
         for (int z = 0; z < length; z++) {
-            x[z] = 50 - z * 10;
-            y[z] = 50;
+            x[z] = B_WIDTH / 2 - z * SQUARE_SIZE    ;
+            y[z] = B_HEIGHT / 2;
         }
         
         locateApple();
@@ -106,38 +96,111 @@ public class Board extends JPanel implements ActionListener {
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        drawGrid(g);
         doDrawing(g);
+    }
+
+    private void drawGrid(Graphics g) {
+        g.setColor(Color.GRAY); // Set the color for the grid lines
+
+        for (int i = 0; i <= B_WIDTH; i += SQUARE_SIZE) {
+            g.drawLine(i, 0, i, B_HEIGHT); // Draw vertical lines
+        }
+
+        for (int j = 0; j <= B_HEIGHT; j += SQUARE_SIZE) {
+            g.drawLine(0, j, B_WIDTH, j); // Draw horizontal lines
+        }
     }
     
     private void doDrawing(Graphics g) {
-        
         if (inGame) {
-
             g.drawImage(apple, appleX, appleY, this);
 
             for (int z = 0; z < length; z++) {
                 if (z == 0) {
-                    g.drawImage(head, x[z], y[z], this);
+                    drawHead(g, x[z], y[z]);
+                } else if (z == length - 1) {
+                    drawTail(g, x[z], y[z]);
                 } else {
-                    g.drawImage(ball, x[z], y[z], this);
+                    drawBodySegment(g, z);
                 }
             }
 
             Toolkit.getDefaultToolkit().sync();
-
         } else {
-
             gameOver(g);
-        }        
+        }
+    }
+
+    private void drawHead(Graphics g, int x, int y) {
+        drawRotatedImage(g, head, x, y, directionToAngle(direction));
+    }
+
+    private void drawTail(Graphics g, int x, int y) {
+        Direction tailDirection = getTailDirection();
+        drawRotatedImage(g, tail, x, y, directionToAngle(tailDirection));
+    }
+
+    private void drawBodySegment(Graphics g, int z) {
+        Direction prevDirection = getDirection(x[z], y[z], x[z - 1], y[z - 1]);
+        Direction nextDirection = getDirection(x[z], y[z], x[z + 1], y[z + 1]);
+
+        if (prevDirection == nextDirection || areOpposite(prevDirection, nextDirection)) {
+            drawRotatedImage(g, bodyStraight, x[z], y[z], directionToAngle(prevDirection));
+        } else {
+            drawRotatedImage(g, bodyBent, x[z], y[z], getBendAngle(prevDirection, nextDirection));
+        }
+    }
+
+    private void drawRotatedImage(Graphics g, Image image, int x, int y, double angle) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.rotate(Math.toRadians(angle), x + SQUARE_SIZE / 2, y + SQUARE_SIZE / 2);
+        g2d.drawImage(image, x, y, this);
+        g2d.rotate(Math.toRadians(-angle), x + SQUARE_SIZE / 2, y + SQUARE_SIZE / 2);
+    }
+
+    private double directionToAngle(Direction direction) {
+        switch (direction) {
+            case UP: return 0;
+            case RIGHT: return 90;
+            case DOWN: return 180;
+            case LEFT: return 270;
+        }
+        return 0;
+    }
+
+    private Direction getDirection(int x1, int y1, int x2, int y2) {
+        if (x1 == x2) {
+            return y1 > y2 ? Direction.UP : Direction.DOWN;
+        } else {
+            return x1 > x2 ? Direction.LEFT : Direction.RIGHT;
+        }
+    }
+
+    private Direction getTailDirection() {
+        if (length < 2) return direction;
+        return getDirection(x[length - 1], y[length - 1], x[length - 2], y[length - 2]);
+    }
+
+    private double getBendAngle(Direction prevDirection, Direction nextDirection) {
+        if ((prevDirection == Direction.UP && nextDirection == Direction.RIGHT) || (prevDirection == Direction.RIGHT && nextDirection == Direction.UP)) {
+            return 0;
+        } else if ((prevDirection == Direction.RIGHT && nextDirection == Direction.DOWN) || (prevDirection == Direction.DOWN && nextDirection == Direction.RIGHT)) {
+            return 90;
+        } else if ((prevDirection == Direction.DOWN && nextDirection == Direction.LEFT) || (prevDirection == Direction.LEFT && nextDirection == Direction.DOWN)) {
+            return 180;
+        } else if ((prevDirection == Direction.LEFT && nextDirection == Direction.UP) || (prevDirection == Direction.UP && nextDirection == Direction.LEFT)) {
+            return 270;
+        }
+        return 0;
     }
 
     private void gameOver(Graphics g) {
         
         String msg = "Game Over";
-        Font small = new Font("Helvetica", Font.BOLD, 14);
+        Font small = new Font("Helvetica", Font.BOLD, 20);
         FontMetrics metr = getFontMetrics(small);
 
         g.setColor(Color.white);
@@ -160,6 +223,7 @@ public class Board extends JPanel implements ActionListener {
             x[z] = x[(z - 1)];
             y[z] = y[(z - 1)];
         }
+
 
         if (direction.equals(Board.Direction.LEFT)) {
             x[0] -= SQUARE_SIZE;
@@ -210,10 +274,10 @@ public class Board extends JPanel implements ActionListener {
 
     private void locateApple() {
         int r = (int) (Math.random() * (B_WIDTH / SQUARE_SIZE));
-        appleX = r * SQUARE_SIZE - SQUARE_SIZE/2;
+        appleX = r * SQUARE_SIZE;
 
         r = (int) (Math.random() * (B_HEIGHT / SQUARE_SIZE));
-        appleY = r * SQUARE_SIZE - SQUARE_SIZE/2;
+        appleY = r * SQUARE_SIZE;
     }
 
     @Override
@@ -227,6 +291,13 @@ public class Board extends JPanel implements ActionListener {
         }
 
         repaint();
+    }
+
+    private boolean areOpposite(Direction dir1, Direction dir2) {
+        return (dir1 == Direction.UP && dir2 == Direction.DOWN) ||
+               (dir1 == Direction.DOWN && dir2 == Direction.UP) ||
+               (dir1 == Direction.LEFT && dir2 == Direction.RIGHT) ||
+               (dir1 == Direction.RIGHT && dir2 == Direction.LEFT);
     }
 
     private class TAdapter extends KeyAdapter {
